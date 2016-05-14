8b517d790a9437e3a4dd87a9e90d8ceb411607be
Darek <d.pienczykowski@selleo.com>
2016-05-14 12:28:04 +0200

    setup material

--- src/demo-app/app.component.ts
+++ src/demo-app/app.component.ts
@@ -3,6 +3,8 @@ import {RouteConfig, Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated
 import {HomeComponent} from  './home/home.component';
 import {LoginComponent} from  './login/login.component';
 import {MdButton} from '@angular2-material/button'
+import {MdIcon} from '@angular2-material/icon'
+import {MdToolbar} from '@angular2-material/toolbar'
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav'
 
 require('../assets/bootstrap.scss');
@@ -13,7 +15,9 @@ require('../assets/bootstrap.scss');
   directives: [
     ROUTER_DIRECTIVES,
     MdButton,
-    MD_SIDENAV_DIRECTIVES
+    MD_SIDENAV_DIRECTIVES,
+    MdToolbar,
+    MdIcon
   ]
 })
 @RouteConfig([
--- src/demo-app/boot.ts
+++ src/demo-app/boot.ts
@@ -1,12 +1,16 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
+import {MdIconRegistry} from '@angular2-material/icon';
 
 document.addEventListener('DOMContentLoaded', function main() {
   bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
+    MdIconRegistry,
     { provide: LocationStrategy, useClass: HashLocationStrategy }
   ])
-    .catch(err => console.error(err));
+      .catch(err => console.error(err));
 });
