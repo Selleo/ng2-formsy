3e6da6dc16bf42785ce6b6ad5b3d90d04f75cc00
Darek <d.pienczykowski@selleo.com>
2016-05-14 17:53:33 +0200

    main page

--- src/demo-app/app.component.ts
+++ src/demo-app/app.component.ts
@@ -4,20 +4,25 @@ import {HomeComponent} from  './home/home.component';
 import {LoginComponent} from  './login/login.component';
 import {LabelsComponent} from  './labels/labels.component';
 import {MdButton} from '@angular2-material/button'
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav'
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list'
+import {MdButton, MdAnchor} from '@angular2-material/button'
 import {MdIcon} from '@angular2-material/icon'
 import {MdToolbar} from '@angular2-material/toolbar'
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav'
 
 require('../assets/bootstrap.scss');
+require('./app.component.scss');
 
 @Component({
   selector: 'demo-app',
   template: require('./app.component.html'),
   directives: [
     ROUTER_DIRECTIVES,
-    MdButton,
     MD_SIDENAV_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MdButton,
     MdToolbar,
+    MdAnchor,
     MdIcon
   ]
 })
